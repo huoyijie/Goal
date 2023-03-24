e496a48b39456015970c8e10b56b922b43849a45
huoyijie <yijie.huo@foxmail.com>
2023-03-24 18:21:18 +0800

    refactor: format code

--- admin/admin.go
+++ admin/admin.go
@@ -7,8 +7,8 @@ import (
 )
 
 type OperationLog struct {
-	ID     uint `goal:"hidden" gorm:"primaryKey"`
-	UserID uint `goal:"hidden"`
+	ID     uint      `goal:"hidden" gorm:"primaryKey"`
+	UserID uint      `goal:"hidden"`
 	User   auth.User `goal:"preload=Username"`
 	Date   time.Time
 	IPAddr,
